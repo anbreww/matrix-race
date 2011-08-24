a625d589d96e01fc8fb94246c346ce69ad0bc533
Andrew Watson <andy@watsons.ch>
2011-08-24 04:16:04 -0700

    initial commit - defines basic program structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# LED Matrix Racing Game
+
+A simple game for 8-bit AVR microcontrollers (ATMEGA164p)
+
+runs on a bi-colour (red-green) 8x8 LED matrix.
+
+## Description
+
+A track (green leds) loops around the display, while the player moves a red
+"car" (one LED) from left to right, avoiding the green track.
+
+## Controls
+
+buttons 2 and 3 (bottom left and bottom right) move the car left and right.
+button 4 pauses the game.
+other controls TBD (controls during pause and after the end of the game).
+
---
+++ main.c
@@ -0,0 +1,64 @@
+/***********************************
+ *
+ *  LED Matrix Racing Game
+ *  Andrew Watson 2011
+ *  
+ **********************************/
+
+#include <avr/io.h>
+#include "matrix/matrix.h"
+#include "track.h"
+
+uint8_t car_pos = 0;
+uint8_t track_pos = 0; // tracks up to 256 lines
+
+int main(void)
+{
+    // LED matrix race game
+    
+    init_matrix();
+
+    /*
+     * BEGINNING OF GAME - inits
+     */
+    game_init();
+    setup_interrupts();
+
+    // Start loop
+    while(flags.game_running)
+    {
+        // reset display
+        matrix_clear();
+
+        // display car
+        matrix_set_line(0,car_pos,RED); // set bottom line with one red dot
+
+        // load 8 track segments
+        for(i=0; i<8; i++)
+        {
+            uint8_t line = 0;
+            // load segment
+            line = LOAD(track_pos + i); // TODO : complete stub
+
+            matrix_set_line(i,line,GREEN);
+        }
+    }
+
+return 0; // never reached
+
+}
+
+void game_init(void)
+{
+    // init car position
+    car_pos = 0b00010000;
+
+    // reset pointer to race track
+    track_pos = 0;
+}
+
+void setup_interrupts(void)
+{
+    // interrupts for left and right buttons
+    // timers for display refreshing and score counting?
+}
