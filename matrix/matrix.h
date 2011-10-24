8fbe7c80f3a3f72f0a0b8b407774d6b08ecea804
Andrew Watson <andy@watsons.ch>
2011-10-24 02:40:27 +0200

    matrix: small documentation fix

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,25 +1,25 @@
 /**
  * @file matrix.h
- * @brief  Functions to draw stuff in the matrix buffer
  * @author Andrew Watson (andy@watsons.ch)
- * @version 0.1
  * @date 2011-10-24
+ * @version 0.1
  *
- *  What do we want to be able to do?
- *  -# Use function 5 (bitmask) for the low level part.
- *  -# Clear the whole matrix
- *  -# Clear only one colour
- *  -# Set a pixel to a given colour, regardless of what colour it is
- *  -# Toggle a pixel (green/off, red/off, red/green)
- *      -# What happens if a pixel is green and we want to toggle red-off?
- *      -# Does it go green-red-off-red, green-off-red-off or green-orange..?
- *  -# Bitmasks on a line (and, or, xor)
- *      -# one mask for green, one mask for red
- *  -# read pixels of a line (red or green), returns uint8
- *  -# read a pixel (returns OFF, RED, GREEN)
+ * @brief  Functions to draw stuff in the matrix buffer.
  *
- *  -# optional : provide the same functionality as lines on the columns
- *  -# to think about : do this directly, or double-buffer?
+ * What do we want to be able to do?
+ * -# Use function 5 (bitmask) for the low level part.
+ * -# Clear the whole matrix
+ * -# Clear only one colour
+ * -# Set a pixel to a given colour, regardless of what colour it is
+ * -# Toggle a pixel (green/off, red/off, red/green)
+ *     -# What happens if a pixel is green and we want to toggle red-off?
+ *     -# Does it go green-red-off-red, green-off-red-off or green-orange..?
+ * -# Bitmasks on a line (and, or, xor)
+ *     -# one mask for green, one mask for red
+ * -# read pixels of a line (red or green), returns uint8
+ * -# read a pixel (returns OFF, RED, GREEN)
+ * -# optional : provide the same functionality as lines on the columns
+ * -# to think about : do this directly, or double-buffer?
  */
 
 #define M_OFF   0   //!< not used
